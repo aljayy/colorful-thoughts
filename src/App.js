371b06e4402cd36fde3215fa67f0a0ccfc3d80a8
Alan Jimenez <stajimenez97@gmai.com>
2022-03-05 20:06:58 -0800

    feat: add hero component and add background color to nav
    Since the card element holding the text lapped over the nav component I needed to add a background color to it.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "./components/Navbar";
-import classes from "./App.modules.scss";
+import styles from "./App.module.scss";
+import Hero from "./components/Hero";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Hero />
     </div>
   );
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import styles from "./Hero.module.scss";
+import heroimg from "../images/smgaiaplatforms.jpg";
+
+function Hero() {
+  return (
+    <section>
+      <div className={styles["hero-card"]}>
+        <div className={styles.top}></div>
+        <div className={styles["top-left"]}></div>
+        <div className={styles.bottom}></div>
+        <div className={styles["bottom-right"]}></div>
+        <h2>Creation of the Month</h2>
+        <p>
+          It comes to no surpise that Stella McCartney’s Gaia Platform’s makes
+          our pick of the month.
+          <br />
+          <br />
+          A rugged and jagged silhouette perfectly balanced by a mango-orange
+          color palette.
+          <br />
+          <br />
+          100% organic cotton
+          <i> for our</i> planet.
+        </p>
+        <button>Shop</button>
+      </div>
+      <div className={styles["img-container"]}>
+        <img src={heroimg} />
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
