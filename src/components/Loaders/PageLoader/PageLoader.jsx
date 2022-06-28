53fff99980fdd736897ff82f9ab7af6b15dd09af
Alan Jimenez <stajimenez97@gmai.com>
2022-06-28 16:24:47 -0700

    feat: add Loader components

---
+++ src/components/Loaders/PageLoader/PageLoader.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./PageLoader.module.scss";
+
+function PageLoader() {
+  return <div className={styles.loader}></div>;
+}
+
+export default PageLoader;
