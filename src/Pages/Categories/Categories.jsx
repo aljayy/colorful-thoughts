e047c66ed1b5cc6cfdaea7c734e362b65b7276fb
Alan Jimenez <stajimenez97@gmai.com>
2022-07-07 16:35:16 -0700

    refactor: make a reusable component to render available categories for both the Category and Designer components

--- src/Pages/Categories/Categories.jsx
+++ src/Pages/Categories/Categories.jsx
@@ -1,50 +1,67 @@
 import React, { useEffect, useState } from "react";
-import Spacer from "../../Components/Spacer/Spacer";
+import { Link, useLocation } from "react-router-dom";
 import styles from "./Categories.module.scss";
 import { commerce } from "../../Auth/commerce";
 import PageLoader from "../../Components/Loaders/PageLoader/PageLoader";
 
 function Categories() {
+  const location = useLocation();
   const [categories, setCategories] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-  useEffect(async () => {
-    const response = await commerce.categories.retrieve("categories", {
-      type: "slug",
-    });
+  const path = location.pathname[1].toUpperCase() + location.pathname.slice(2);
 
-    setCategories(
-      response.children
-        .map((category) => {
-          return {
-            name: category.name,
-            id: category.id,
-          };
-        })
-        .sort((a, b) => {
-          if (a.name < b.name) {
-            return -1;
-          } else if (a.name > b.name) {
-            return 1;
-          }
+  console.log(path);
 
-          return 0;
-        })
-    );
-  }, []);
+  useEffect(() => {
+    async function fetchData() {
+      setLoading(true);
+      const response = await commerce.categories.retrieve(path.toLowerCase(), {
+        type: "slug",
+      });
 
-  console.log(categories);
+      setCategories(
+        response.children
+          .map((category) => {
+            return {
+              name: category.name,
+              id: category.id,
+            };
+          })
+          .sort((a, b) => {
+            if (a.name < b.name) {
+              return -1;
+            } else if (a.name > b.name) {
+              return 1;
+            }
+
+            return 0;
+          })
+      );
+      setLoading(false);
+    }
+    fetchData();
+  }, [path]);
+
   return (
-    <Spacer>
-      {categories.length < 1 && <PageLoader />}
-      {categories.length >= 1 && (
+    <section className={styles.category}>
+      {(categories.length < 1 && <PageLoader />) || (loading && <PageLoader />)}
+      {categories.length >= 1 && !loading && (
         <>
-          <h2 className={styles["category-h2"]}>Categories</h2>
+          <h2 className={styles["category-h2"]}>{path}</h2>
           {categories.map((category) => {
-            return <h3 className={styles["category-h3"]}>{category.name}</h3>;
+            return (
+              <Link
+                to={`/${path.toLowerCase()}/${category.name.toLowerCase()}`}
+                style={{ textDecoration: "none" }}
+              >
+                <h3 className={styles["category-h3"]}>{category.name}</h3>
+              </Link>
+            );
           })}
         </>
       )}
-    </Spacer>
+    </section>
   );
 }
 
