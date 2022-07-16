a6dce48362d134c492b6658fad5267ce1ce07bbb
Alan Jimenez <stajimenez97@gmai.com>
2022-07-16 14:28:25 -0700

    refactor: move data fetching Logic from App.js to HomePageProducts

--- src/App.js
+++ src/App.js
@@ -1,61 +1,16 @@
-import React, { useEffect, useState } from "react";
-import { commerce } from "./Auth/commerce";
+import React from "react";
 import styles from "./App.module.scss";
 import Navbar from "./Components/Navbar/Navbar";
 import RoutesProvider from "./Pages/RoutesProvider";
 import Footer from "./Components/Footer/Footer";
 
 function App() {
-  const [homeProducts, setHomeProducts] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  let categoryArr = [];
-
-  async function getCategories() {
-    const categoryResponse = await commerce.categories.retrieve("designers", {
-      type: "slug",
-    });
-
-    for (let i = 0; i < categoryResponse.children.length; i++) {
-      const productResponse = await commerce.products.list({
-        category_id: categoryResponse.children[i].id,
-        limit: 6,
-      });
-
-      const productData = [];
-
-      productData.push(
-        productResponse.data.map((product) => {
-          return {
-            productName: product.name,
-            productImage: product.image.url,
-            productPrice: product.price.raw,
-            productId: product.id,
-          };
-        })
-      );
-
-      categoryArr.push({
-        categorySlug: categoryResponse.children[i].slug,
-        id: categoryResponse.children[i].id,
-        heroImg: categoryResponse.children[i].assets[0].url,
-        products: productData,
-      });
-    }
-    setHomeProducts(categoryArr);
-    setLoading(false);
-  }
-
-  useEffect(() => {
-    getCategories();
-  }, []);
-
   return (
-    <div>
+    <>
       <Navbar />
-      <RoutesProvider loading={loading} homeProducts={homeProducts} />
+      <RoutesProvider />
       <Footer />
-    </div>
+    </>
   );
 }
 
