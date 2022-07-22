20bb07f1d18b49fbd9e2626dff6feed3734b0f8b
Alan Jimenez <stajimenez97@gmai.com>
2022-07-22 13:18:43 -0700

    refactor: refactor routes and props being passed into RoutesProvider component

--- src/Pages/RoutesProvider.jsx
+++ src/Pages/RoutesProvider.jsx
@@ -1,34 +1,32 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import Hero from "../Components/Hero/Hero";
 import HomePageProducts from "../Components/Products/HomePageProducts/HomePageProducts";
-import DesignerCategory from "./DesignerCategory/DesignerCategory";
 import UserDashboard from "./UserDashboard/UserDashboard";
-import IndividialProduct from "../Components/Products/IndividualProduct/IndividualProduct";
 import Bag from "./Bag/Bag";
 import About from "./About/About";
 import LoginForm from "./Login/LoginForm";
 import SignUpForm from "./SignUp/SignUpForm";
 import Categories from "./Categories/Categories";
+import IndividualProduct from "../Components/Products/IndividualProduct/IndividualProduct";
+import CategoryProducts from "./Categories/CategoryProducts/CategoryProducts";
+import DesignerProducts from "./Categories/DesignerProducts/DesignerProducts";
 
-function RoutesProvider({ loading, homeProducts }) {
+function RoutesProvider() {
   return (
     <Routes>
-      <Route
-        path="/home"
-        element={[
-          <Hero />,
-          <HomePageProducts products={homeProducts} loading={loading} />,
-        ]}
-      />
-      <Route path="/designers/:categorySlug" element={<DesignerCategory />} />
-      <Route path="/product/:productId" element={<IndividialProduct />} />
-      <Route path="/dashboard/" element={<UserDashboard />} />
+      <Route path="/" element={<Navigate to="/home" />} />
+      <Route path="/home" element={[<Hero />, <HomePageProducts />]} />
       <Route path="/about/" element={<About />} />
-      <Route path="/mybag/" element={<Bag />} />
-      <Route path="/login/" element={<LoginForm />} />
-      <Route path="/signup/" element={<SignUpForm />} />
       <Route path="/categories/" element={<Categories />} />
+      <Route path="/categories/:categorySlug" element={<CategoryProducts />} />
+      <Route path="/dashboard/" element={<UserDashboard />} />
+      <Route path="/designers" element={<Categories />} />
+      <Route path="/designers/:categorySlug" element={<DesignerProducts />} />
+      <Route path="/login/" element={<LoginForm />} />
+      <Route path="/mybag/" element={<Bag />} />
+      <Route path="/product/:productId" element={<IndividualProduct />} />
+      <Route path="/signup/" element={<SignUpForm />} />
     </Routes>
   );
 }
